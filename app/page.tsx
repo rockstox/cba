2628335406d686abcda21b011acacc9935c28159
Hunter <huntleydavis3@gmail.com>
2023-06-29 19:56:24 +0200

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,54 @@
 "use client";
 
 import { useChat } from "ai/react";
+import { setHttpClientAndAgentOptions } from "next/dist/server/config";
+import { useState } from "react";
 
 export default function Home() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  const { messages, input, handleInputChange, setInput, handleSubmit } = useChat(); 
+  const [promptValue, setPromptValue] = useState("");
+
+  const examplePrompts = [
+    "What is the salary cap for the new season?",
+    "Explain the luxury tax in the new NBA CBA.",
+    "What changes were made in player trades?",
+    "Can a team use the Non-Taxpayer Mid-Level Salary Exception to sign a player who was previously waived by another team during the same Salary Cap Year?"
+  ];
+
+  const handlePromptClick = (prompt: string) => {
+    setInput(prompt);
+  };
+
 
   return (
     <div className="mx-auto w-full max-w-[600px] py-2 sm:py-10 flex flex-col stretch text-neutral-50 px-2">
       <div className="text-center">
-        <div className="text-[60px]">🏀</div>
-        <div className="text-4xl font-bold">NBA CBA Chat</div>
-        <div className="mt-3 text-xl">Use AI to ask questions about the new 676-page NBA CBA.</div>
-        <div className="mt-2">Please donate to help us keep the tool online for everyone :)</div>
-        <button
-          className="bg-green-500 hover:bg-green-700 text-white font-bold p-2 rounded h-[40px] mt-4"
-          onClick={() => window.open("https://donate.stripe.com/5kAeWqezzdn6gtWaEF")}
-        >
-          DUNK ON LEBRON
-        </button>
+        <div className="text-4xl font-bold">Explore the new NBA CBA</div>
+        <div className="mt-3 text-xl">Interact with AI to understand the latest 676-page NBA Collective Bargaining Agreement (CBA).</div>
       </div>
 
-      {messages.length > 0 ? (
-        <div className="flex flex-col mt-5 border-b-2 mb-20">
+      {messages.length === 0 && (
+        <div className="my-5">
+        <div className="text-2xl text-center font-bold mb-4">Example Prompts</div>
+        {examplePrompts.map((prompt, i) => (
+          <button
+            key={i}
+            className="bg-gray-300 hover:bg-gray-400 text-black font-medium p-2 rounded mb-2 w-full text-left"
+            onClick={() => handlePromptClick(prompt)}
+          >
+            {prompt}
+          </button>
+        ))}
+      </div>
+      )}
+      
+      {messages.length > 0 && (
+        <div className="flex flex-col mt-y border-b-2">
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded h-[40px] flex justify-center mb-4"
             onClick={() => window.location.reload()}
           >
-            Ask new question
+            Ask Another Question
           </button>
 
           {messages.map((m) => (
@@ -39,7 +61,7 @@ export default function Home() {
             </div>
           ))}
         </div>
-      ) : null}
+      )}
 
       <form
         className="flex fixed bottom-0 sm:bottom-4"
@@ -57,7 +79,7 @@ export default function Home() {
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded h-[40px] ml-2"
           type="submit"
         >
-          Send
+          Ask
         </button>
       </form>
     </div>
